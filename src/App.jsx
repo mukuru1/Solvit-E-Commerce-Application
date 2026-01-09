dbb5231d3db4a38a78dbb5e14a11672d9cde1f19
mukuru1 <kabengaedwin@gmail.com>
2026-01-09 12:43:14 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,39 @@
-import { useState } from 'react'
+import { AuthProvider } from "./contexts/AuthContext";
+import { CartProvider } from "./contexts/cartContext";
+import { Header } from "./components/header";
+import { ProductList } from "./components/productList";
+import { Cart } from "./pages/Cart";
+import { Wishlist } from "./pages/Wishlist";
+import { Login } from "./pages/Login";
+import { Dashboard } from "./pages/Dashboard";
+import { Route, ProtectedRoute } from "./pages/Router";
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <CartProvider>
+        <Header />
+
+        <Route path="/">
+          <ProductList />
+        </Route>
+
+        <Route path="/cart">
+          <Cart />
+        </Route>
+
+        <Route path="/wishlist">
+          <Wishlist />
+        </Route>
+
+        <Route path="/login">
+          <Login />
+        </Route>
+
+        <ProtectedRoute path="/dashboard">
+          <Dashboard />
+        </ProtectedRoute>
+      </CartProvider>
+    </AuthProvider>
+  );
+}
